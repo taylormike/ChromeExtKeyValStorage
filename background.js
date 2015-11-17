2a53057ce4143f308d16de9aac21c3a4a0277c00
taylormike <mike.j.taylor0@gmail.com>
2015-11-17 00:53:43 -0500

    removing comments

--- background.js
+++ background.js
@@ -1,10 +1,3 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
-
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
-
-
 chrome.extension.onMessage.addListener(
   function (request, sender, sendResponse) {
       if (request.action == "getStorage") {
